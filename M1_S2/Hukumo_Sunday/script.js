069f5ca0a6a01f055e2fa5f0c59e0935c926a406
kartik <work.kartikgarg@gmail.com>
2025-03-24 11:27:44 +0530

    hukumo

---
+++ M1_S2/Hukumo_Sunday/script.js
@@ -0,0 +1,25 @@
+function check (str, element){
+
+    let cnt=0;
+    for(let i=0; i<str.length-element.length; i++){
+        let result = "";
+        for(let j=0; j<element.length; j++){
+            if(str[i+j] == element[j]){
+                result += element[j];
+            }
+            else{
+                break;
+            }
+        }
+        if(result==element){
+            cnt++;
+        }
+    }
+    return cnt;
+
+}
+
+let str1 = "My name is kartik name"
+let str2 = "kartik check your name it is kartik or kartik";
+console.log(check(str1, "name"));
+console.log(check(str2, "kartik"));
