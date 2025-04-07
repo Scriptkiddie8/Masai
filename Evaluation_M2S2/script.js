72adee36f8a6b1e4d7c39624efa360f02507a925
kartik <work.kartikgarg@gmail.com>
2025-04-07 17:54:37 +0530

    checkevaluation

--- Evaluation_M2S2/script.js
+++ Evaluation_M2S2/script.js
@@ -2,104 +2,148 @@ function createStudentManager(){
 
     let student = {};
 
-    return{
-    addStudent: function(id, name){
-        if(!student[id]){
-            student[id] = {
-                id,
-                name,
-                subjects : {},
-            };
-            return student[id];
-        }
-        else{
-            console.log(`Student with ${id} already presnt`);
-        }
-    },
-
-    addSubject: function(id, subject){
-        if(!student[id]){
-            console.log(`Student with ${id} not found`)
-        }
-        else{
-            if(!(subject in student[id].subjects)){
-                student[id].subjects[subject] = null; 
+    return {
+        addstudent(id, name){
+            if(!student[id]){
+                student[id] = {
+                    id,
+                    name,
+                    subjects : {},
+                    avg_marks : null,
+                }
+                return student[id];
             }
             else{
-                console.log(`${subject} subject already present`);
+                console.log(`Student with ${id} already exist`);
             }
-        }
-    },
+        },
 
-    updateScore: function(id, subject, score){
-        if(!student[id]){
-            console.log(`Student with ${id} not found`)
-        }
-        else{
-            if(!(subject in student[id].subjects)){
-                console.log(` ${subject} subject not present`);
+        addsubject(id, subject){
+            if(!student[id]){
+                console.log(`student with ID:${id}  does not exist`)
+            }
+            if(subject in student[id].subjects){
+                console.log(`${subject} is already added`)
             }
             else{
-                student[id].subjects[subject] = score;
+                student[id].subjects[subject] = null
             }
-        }
-    },
+        },
 
-    getStudentDetails: function(id){
-        if(!student[id]){
-            console.log(`Student with ${id} not found`)
-        }
-        else{
-            return student;
-        }
-    },
+        updatescore(id, subject, score){
+            if(!student[id]){
+                console.log(`student with ID:${id}  does not exist`)
+            }
+            if(!(subject in student[id].subjects)){
+                console.log(`${subject} not found`)
+            }
+            else{
+                student[id].subjects[subject] = score
+            }
 
-    averageMarks: function(id){
-        if(!(subject in student[id].subjects)){
-            console.log(`Had no subjects to calculate`)
+        },
+
+        getstudentdetail(id){
+            if(!student[id]){
+                console.log(`Student with ID:${id} does not exist`)
+            }
+            else{
+                return student[id];
+            }
+        },
+
+        getaveragemarks(id){
+            if(!student[id]){
+                console.log(`Student with ID:${id} does not exist`)
+            }
+            else{
+                let marks = Object.values(student[id].subjects)
+                let total = marks.reduce((acc, curr)=>acc+=curr, 0)
+                let avg = total/Object.keys(student[id].subjects).length
+                student[id].avg_marks = avg;
+            }
+        },
+
+        gettopstudent(){
+            let max = -Infinity;
+            let topid = null;
+            for(let id in student){
+                if(student[id].avg_marks !=null && student[id].avg_marks > max){
+                    max = student[id].avg_marks;
+                    topid = student[id];
+                }
+            }
+            console.log(`${topid.name} is topper with avg score of ${max}`)
+            
+        },
+
+        getdifficultsubject(){
+            let difficult = {};
+                for(let id in student){
+                    for(let subject in student[id].subjects){
+                        let marks = Object.values(student[id].subjects[subject]);
+                        if(marks<40){
+                            if(difficult[subject]){
+                                difficult[subject]++;
+                            }
+                            else{
+                                difficult[subject] = 1;
+                            }
+                        }
+                    }
+                }
+                let half = Object.keys(student).length/2;
+                for(let subject in difficult){
+                    let value = Object.values(difficult[subject])
+                    if(value > half){
+                        console.log(`${difficult[subject]} is difficult`)
+                    }
+                }
+        },
+
+        getfailedstudent(){
+            for(let id in student){
+                for(let subject in student[id].subjects){
+                    let marks = Object.values(student[id].subjects[subject]);
+                    if(marks<35){
+                        console.log(`${student[id].name} is failed in ${subject}`)
+                        break;
+                    }
+                }
+            }
+        },
+
+        enrolledsubjects(){
+            let frequency = {};
+            for(let id in student){
+                for(let subject in student[id].subjects){
+                    if(frequency[subject]){
+                        frequency[subject]++;
+                    }
+                    else{
+                        frequency[subject] = 1;
+                    }
+                }
+            }
+            console.log(frequency)
         }
-        else{
-            let marks =  Object.values(student[id].subjects);
-            let total_marks = marks.reduce((acc, curr)=>{acc+curr}, 0);
-            let avg = total_marks/marks.length;
-            console.log(`Average marks are ${avg}`);
-        }
+
     }
-
-    }
-
+   
 }
 
+let config = createStudentManager();
 
-let add = createStudentManager();
-let student1 = add.addStudent("1", "Kartik");
-add.addSubject("1", "JavaScript")
-add.addSubject("1", "C++")
-add.addSubject("1", "DSA")
-add.updateScore("1", "JavaScript", 99)
-add.updateScore("1", "C++", 100)
-add.updateScore("1", "DSA", 100)
-console.log(student1);
+config.addstudent(1, "Kartik");
+config.addsubject(1, "Math");
+config.addsubject(1, "English");
+config.updatescore(1, "English", 100)
+config.updatescore(1, "Math", 29)
+config.getaveragemarks(1)
+let student1 = config.getstudentdetail(1)
+console.log(student1)
+config.gettopstudent()
+config.getfailedstudent()
+config.getdifficultsubject()
+config.enrolledsubjects()
 
-let student2 = add.addStudent("2", "Jack");
-add.addSubject("2", "JavaScript")
-add.addSubject("2", "C++")
-add.addSubject("2", "DSA")
-add.updateScore("2", "JavaScript", 30)
-add.updateScore("2", "C++", 19)
-add.updateScore("2", "DSA", 55)
-console.log(student2);
-
-
-
-let student3 = add.addStudent("3", "Richard");
-add.addSubject("3", "JavaScript")
-add.addSubject("3", "C++")
-add.addSubject("3", "DSA")
-add.updateScore("3", "JavaScript", 78)
-add.updateScore("3", "C++", 23)
-add.updateScore("3", "DSA", 88)
-console.log(student3);
-
-
-console.log(student1.averageMarks)
