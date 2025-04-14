ec5bd5978409d652c8f1838907f6b578cd4edb34
kartik <work.kartikgarg@gmail.com>
2025-04-14 16:48:50 +0530

    notes

--- Notes/setTimeout/script.js
+++ Notes/setTimeout/script.js
@@ -1,10 +1,10 @@
 //what will be the output in this case?
 
 function check1(){
-    setTimeout(()=>(console.log("kartik"),8000))//9s
+    setTimeout(()=>console.log("kartik"),8000)//9s
 }
 function check2(){
-    setTimeout(()=>(console.log("garg"),9000))//7s
+    setTimeout(()=>console.log("garg"),5000)//7s
 }
-setTimeout(()=>check1(),5000)
-setTimeout(()=>check2(),1000)
+setTimeout(()=>check1(),1000)
+setTimeout(()=>check2(),2000)
