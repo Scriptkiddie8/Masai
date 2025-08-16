e132733d13cee52de588d3c416cbeedfe424f965
kartik <work.kartikgarg@gmail.com>
2025-08-16 12:12:34 +0530

    Custom-Hook

---
+++ Unit-4/Custom Hook/my-react-app/src/components/MyForm.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import useFormInput from "../hooks/useFormInput";
+
+function MyForm() {
+  const name = useFormInput("");
+  const email = useFormInput("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Name: ${name.value}, Email: ${email.value}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Name:</label>
+        <input type="text" {...name} />
+      </div>
+      <div>
+        <label>Email:</label>
+        <input type="email" {...email} />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default MyForm;
