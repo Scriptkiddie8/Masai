310c4721cf00934118cefe9c58378c4a484e8b8c
kartik <work.kartikgarg@gmail.com>
2025-08-16 17:09:06 +0530

    Context API Implementation

--- React-Notes/Context API in React and ChakraUI/my-react-app/src/App.jsx
+++ React-Notes/Context API in React and ChakraUI/my-react-app/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { createContext, useContext } from "react";
+
+//Step:1 - Create Context
+const UserContext = createContext();
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState("Kittu");
 
+  //Step:2 - Provide Context
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <UserContext.Provider value={user}>
+      <Parent />
+    </UserContext.Provider>
+  );
 }
 
-export default App
+function Parent() {
+  return <Child />;
+}
+
+function Child() {
+  return <GrandChild />;
+}
+
+function GrandChild() {
+  //Step:3 - use Context (Final step)
+  const user = useContext(UserContext);
+  return <h1>Hello, {user}</h1>;
+}
+
+export default App;
