f2daea0b7756a5f332b8b5c9ba346a036172803f
kartik <work.kartikgarg@gmail.com>
2025-08-16 12:20:23 +0530

    Custom Timer Hook

---
+++ Unit-4/useTimer Custom Hook/my-react-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import TimerComponent from "./components/TimerComponent";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Custom useTimer Hook</h1>
+      <TimerComponent />
+    </div>
+  );
+}
+
+export default App;
