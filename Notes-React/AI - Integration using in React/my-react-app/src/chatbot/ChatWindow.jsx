97feab4a91729a85c2d27ce2353198c106ce7dd9
kartik <work.kartikgarg@gmail.com>
2025-08-19 18:50:31 +0530

    First AI- Integrated App(Chatbot)

--- Notes-React/AI - Integration using in React/my-react-app/src/chatbot/ChatWindow.jsx
+++ Notes-React/AI - Integration using in React/my-react-app/src/chatbot/ChatWindow.jsx
@@ -1,11 +1,18 @@
-import React from "react"
+import React from "react";
 import { GoogleGenAI } from "@google/genai";
-import { Sparkle, Maximize2, X, Send, LoaderPinwheel } from "lucide-react"; 
+import {
+  Sparkle,
+  Maximize2,
+  Minimize2,
+  X,
+  Send,
+  LoaderPinwheel,
+} from "lucide-react";
 import { useState, useRef, useEffect } from "react";
 import ChatMessage from "./ChatMessage";
 
 const genAI = new GoogleGenAI({
-  apiKey: "AIzaSyA_a7a6lRsUKBEG4_I7i8hN8WLQAVqmE9s", 
+  apiKey: "AIzaSyA_a7a6lRsUKBEG4_I7i8hN8WLQAVqmE9s",
 });
 
 const ChatWindow = ({ isOpen, onClose }) => {
@@ -30,7 +37,9 @@ const ChatWindow = ({ isOpen, onClose }) => {
       setUserInitial((userDetails.name || "G").charAt(0).toUpperCase());
       setMessage([
         {
-          text: `Hi, ${userDetails.name || "Guest"}! I'm your AI assistant powered by Masai. How can I help you today?`,
+          text: `Hi, ${
+            userDetails.name || "Guest"
+          }! I'm your AI assistant powered by Masai. How can I help you today?`,
           isBot: true,
         },
       ]);
@@ -46,7 +55,7 @@ const ChatWindow = ({ isOpen, onClose }) => {
     const handleClickOutside = (event) => {
       if (
         ChatWindowRef.current &&
-        !ChatWindowRef.current.contains(event.target) 
+        !ChatWindowRef.current.contains(event.target)
       ) {
         onClose();
       }
@@ -57,54 +66,56 @@ const ChatWindow = ({ isOpen, onClose }) => {
     }
 
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside); 
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [isOpen, onClose]);
 
   //This will help us to scroll to the message
-  useEffect(()=>{
-    messageEndRef.current?.scrollIntoView({behavior: "smooth"})
+  useEffect(() => {
+    messageEndRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [message]);
 
   //This will focus to input box when the chatbot is open
-  useEffect(()=>{
-    if(isOpen && inputRef.current){
+  useEffect(() => {
+    if (isOpen && inputRef.current) {
       inputRef.current.focus();
     }
-  },[isOpen])
+  }, [isOpen]);
 
-  const handleSubmit = async(e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if(!input.trim() || isLoading) return;
+    if (!input.trim() || isLoading) return;
 
     const userMessage = input.trim();
     setInput("");
-    setMessage((prev)=>[...prev, {text: userMessage, isBot: false}]);
+    setMessage((prev) => [...prev, { text: userMessage, isBot: false }]);
     setIsLoading(true);
 
     try {
       const result = await genAI.models.generateContent({
         model: "gemini-2.5-flash",
         contents: userMessage,
-      })
-      
-      const reply = result.text;
-      setMessage((prev)=>[...prev, {text: reply, isBot: true}]);
+      });
 
+      const reply = result.text;
+      setMessage((prev) => [...prev, { text: reply, isBot: true }]);
     } catch (error) {
       console.log(error);
-      setMessage(prev=> [...prev, {text: "I am sorry, I ran into an error", isBot: true}])
-    }finally{
+      setMessage((prev) => [
+        ...prev,
+        { text: "I am sorry, I ran into an error", isBot: true },
+      ]);
+    } finally {
       setIsLoading(false);
     }
-  }
+  };
 
-  const handleKeyDown = (e)=>{
-    if(e.key === "Enter" && !e.shiftKey){
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && !e.shiftKey) {
       e.preventDefault();
       handleSubmit(e);
     }
-  }
+  };
 
   if (!isOpen) return null;
 
@@ -137,27 +148,30 @@ const ChatWindow = ({ isOpen, onClose }) => {
           >
             {isMinimized ? <Maximize2 size={16} /> : <Minimize2 size={16} />}
           </button>
-          <button onClick={onClose}  className="p-1.5 hover:bg-white/10 rounded-full transition-colors">
-            <X size={16}/>
+          <button
+            onClick={onClose}
+            className="p-1.5 hover:bg-white/10 rounded-full transition-colors"
+          >
+            <X size={16} />
           </button>
         </div>
       </div>
 
       {!isMinimized && (
         <>
-        {/* message */}
+          {/* message */}
           <div className="h-[calc(100%-8rem)] overflow-y-auto p-3 space-y-3 bg-gray-900">
-            {message.map((message, index)=>(
+            {message.map((message, index) => (
               <div key={index} className="flex item-start gap-2 text-white">
                 {message.isBot ? (
-                  <ChatMessage message={message.text} isBot={true}/>
+                  <ChatMessage message={message.text} isBot={true} />
                 ) : (
                   <div className="flex items-start gap-2 justify-end w-full text-white">
                     <div className="flex-1">
-                        <ChatMessage message={message.text} isBot={false}/>
+                      <ChatMessage message={message.text} isBot={false} />
                     </div>
                     <div className="w-6 h-6 rounded-full bg-gradient-to-r from-indigo-500 to-purple-600 text-white p-3 flex items-center justify-between flex items-center justify-center flex-shrink-0 text-xs">
-                        {userInitial}
+                      {userInitial}
                     </div>
                   </div>
                 )}
@@ -165,23 +179,40 @@ const ChatWindow = ({ isOpen, onClose }) => {
             ))}
             {isLoading && (
               <div className="flex items-center gap-2 text-white p-2">
-                <LoaderPinwheel className="w-4 h-4 animate-spin"/>
+                <LoaderPinwheel className="w-4 h-4 animate-spin" />
                 <span className="text-xs">AI is Thinking...</span>
-              </div>)}
+              </div>
+            )}
             <div ref={messageEndRef}></div>
           </div>
 
-          <form onSubmit={handleSubmit} className="p-3 bg-white dark:bg-gray-800 border-t border-gray-100 dark:border-gray-700">
+          <form
+            onSubmit={handleSubmit}
+            className="p-3 bg-white dark:bg-gray-800 border-t border-gray-100 dark:border-gray-700"
+          >
             <div className="relative">
-              <textarea ref={inputRef} value={input} onChange={(e)=>setInput(e.target.value)} onKeyDown={handleKeyDown} rows={1} style={{maxHeight:"100px"}} placeholder="Type your query" className={`w-full pr-10 pl-3 py-2 rounded-xl border-gray-200 text-white`}></textarea>
-              <button type="submit" disabled={isLoading || !input.trim()} className={`absolute left-2 top-1/2 text-white -translate-y-1/2 p-1.5 rounded-full`}>
-                  <Send size={14}/>
+              <textarea
+                ref={inputRef}
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                onKeyDown={handleKeyDown}
+                rows={1}
+                style={{ maxHeight: "100px" }}
+                placeholder="Type your query"
+                className={`w-full pr-10 pl-3 py-2 rounded-xl border-gray-200 text-white`}
+              ></textarea>
+              <button
+                type="submit"
+                disabled={isLoading || !input.trim()}
+                className={`absolute left-2 top-1/2 text-white -translate-y-1/2 p-1.5 rounded-full`}
+              >
+                <Send size={14} />
               </button>
             </div>
           </form>
         </>
       )}
-    </div> 
+    </div>
   );
 };
 
