4a692899b83f64628ca635beb78930601cb20d3d
kartik <work.kartikgarg@gmail.com>
2025-12-07 19:49:12 +0530

    Splitify

--- Splitify/src/config/firebase.js
+++ Splitify/src/config/firebase.js
@@ -1,10 +1,24 @@
+// src/config/firebase.js
 const admin = require("firebase-admin");
 const path = require("path");
 
-const serviceAccount = require(path.join(
-  __dirname,
-  "../../serviceAccountKey.json"
-));
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
+  // Production / deployment: read from env var (Vercel, Render, etc.)
+  try {
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+  } catch (err) {
+    console.error("Failed to parse FIREBASE_SERVICE_ACCOUNT_KEY:", err);
+    throw err;
+  }
+} else {
+  // Local development: read from local file (ignored by git)
+  serviceAccount = require(path.join(
+    __dirname,
+    "../../serviceAccountKey.json"
+  ));
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
